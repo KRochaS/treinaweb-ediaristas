bdfa9dcd1c5bd567c215961313d2d2261f498862
KRochaS <karinerocha.s@hotmail.com>
2021-06-12 19:16:19 -0300

    SafeEnvironment.tsx

---
+++ ediaristas/src/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,10 @@
+import { SafeEnvironmentContainer } from './SafeEnvironment.style';
+
+
+export function SafeEnvironment() {
+    return(
+        <SafeEnvironmentContainer>
+            Ambiente Seguro
+        </SafeEnvironmentContainer>
+    )
+}
