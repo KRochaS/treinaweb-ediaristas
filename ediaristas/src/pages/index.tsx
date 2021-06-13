e490c9d00d0e83de39df47f89d7ae3f42fbbe3a6
KRochaS <karinerocha.s@hotmail.com>
2021-06-13 17:06:55 -0300

    index

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,10 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { SafeEnvironment } from '../components/feedback/SafeEnvironment/SafeEnvironment';
-
+import { PageTitle } from '../components/data-display/PageTitle/PageTitle';
 
 export default function Home() {
-	return (
-		<SafeEnvironment />
-	)
+    return (
+        <div>
+            <SafeEnvironment />
+            <PageTitle
+                title={'Conheça os profissionais'}
+                subtitle={'Preencha seu endereço e veja  todos os profissionais da sua localidade'}
+
+            />
+        </div>
+    )
 }
