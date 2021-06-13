b07f8f646a4993839e378f54e95a4b7d5b6289ec
KRochaS <karinerocha.s@hotmail.com>
2021-06-13 17:06:34 -0300

    SafeEnvirnment

--- ediaristas/src/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
+++ ediaristas/src/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -2,8 +2,7 @@ import { experimentalStyled as styled } from '@material-ui/core/styles';
 
 
 export const SafeEnvironmentContainer = styled('div')`
-    background-color: red;
-    color: white;
+    color: black;
     text-align: right;
     padding: 16px 0;
     font-size: 12px;
