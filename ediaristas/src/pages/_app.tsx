27b9fc7ad4d3216c92d8383c0612b9e4698c16a9
KRochaS <karinerocha.s@hotmail.com>
2021-06-14 19:49:57 -0300

    _app

--- ediaristas/src/pages/_app.tsx
+++ ediaristas/src/pages/_app.tsx
@@ -2,12 +2,21 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@material-ui/core';
 import theme from '../themes/theme';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <>
+            <Head>
+                <title> e-diaristas </title>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
+                <link href="/fonts/tw-icons/css/treinaweb-icons.css" rel="stylesheet" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
     )
 
 }
